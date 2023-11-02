5290851a528b3f79a41f5e73796021ab651b04ba
Galitie <galitdances@gmail.com>
2023-11-02 02:32:56 -0400

    getting dynamic content back

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import clsx from 'clsx'
 
 import { Button } from '@/components/Button'
+import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
 import {
   GitHubIcon,
@@ -18,6 +19,8 @@ import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
 import image4 from '@/images/photos/image-4.jpg'
 import image5 from '@/images/photos/image-5.jpg'
+import { getAllArticles } from '@/lib/articles'
+import { formatDate } from '@/lib/formatDate'
 
 function MailIcon(props) {
   return (
@@ -75,6 +78,21 @@ function ArrowDownIcon(props) {
         strokeLinejoin="round"
       />
     </svg>
+  )
+}
+
+function Article({ article }) {
+  return (
+    <Card as="article">
+      <Card.Title href={`/articles/${article.slug}`}>
+        {article.title}
+      </Card.Title>
+      <Card.Eyebrow as="time" dateTime={article.date} decorate>
+        {formatDate(article.date)}
+      </Card.Eyebrow>
+      <Card.Description>{article.description}</Card.Description>
+      <Card.Cta>Read article</Card.Cta>
+    </Card>
   )
 }
 
@@ -214,6 +232,8 @@ function Photos() {
 }
 
 export default async function Home() {
+  let articles = (await getAllArticles()).slice(0, 4)
+
   return (
     <>
       <Container className="mt-9">
@@ -266,8 +286,15 @@ export default async function Home() {
       </Container>
       <Photos />
       <Container className="mt-24 md:mt-28">
-        <div className="space-y-10 lg:pl-16 xl:pl-24">
-          <Resume />
+        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+          <div className="flex flex-col gap-16">
+            {articles.map((article) => (
+              <Article key={article.slug} article={article} />
+            ))}
+          </div>
+          <div className="space-y-10 lg:pl-16 xl:pl-24">
+            <Resume />
+          </div>
         </div>
       </Container>
     </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,6 +23,7 @@ export function Footer() {
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/projects">Portfolio</NavLink>
+                <NavLink href="/articles">Posts</NavLink>
                 <NavLink href="/games">Games</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
